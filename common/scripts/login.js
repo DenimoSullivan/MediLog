fbd57566d98e9b38050e2c933d205c875d5c33ca
sirrolfe <125940085+sirrolfe@users.noreply.github.com>
2024-01-07 18:46:35 +0700

    Update login.js

--- common/scripts/login.js
+++ common/scripts/login.js
@@ -15,24 +15,20 @@ document.querySelectorAll('.radio-child input').forEach(element => {
 
 function redirect(e) {
     e.preventDefault();
-    // Check if any of the role radio buttons is checked
     const selectedRoleRadio = document.querySelector('input[name="role"]:checked');
     if (!selectedRoleRadio) {
         alert('Please select a role.');
         return;
     }
     
-    // Get the value of the selected radio button
     const selectedRole = selectedRoleRadio.value;
     
-    // Define the redirection paths for each role
     const paths = {
         'admin': '../../admin/pages/home.html',
         'doctor': '../../dokter/pages/home.html',
         'nurse': '../../suster/pages/home.html'
     };
 
-    // Check if the selected role has a defined redirection path
     const page = paths[selectedRole];
     if (!page) {
         alert('The selected role does not have an associated page.');
